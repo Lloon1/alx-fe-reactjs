660d4acacdfe842615a1b05a45d2a78310254e1f
Lena Mustafa <Lena24Mustafa@gmail.com>
2024-09-15 19:38:41 +0400

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,16 @@
 
 import './App.css'
 import './index.css';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
-  <h1>Hello from learning Tailwind Css</h1>
+  return (
+    //<h1>Hello from learning Tailwind Css</h1>
+    <div>
+        <UserProfile />
+    </div>
+  );
 }
 
 export default App
