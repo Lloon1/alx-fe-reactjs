17a8cdb03376a7493e5dd4f127ebecce482a19d8
Lena Mustafa <Lena24Mustafa@gmail.com>
2024-09-15 18:22:02 +0400

    Tailwind CSS purge configuration

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [],
-}
+};
 
